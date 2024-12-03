a1b9090f812bc5ab2b8c6e3d3359bd9d3020470c
daniel daskal <daskal.daniel@gmail.com>
2024-12-03 21:02:26 +0200

    task comleted

--- Primes.java
+++ Primes.java
@@ -1,5 +1,43 @@
 public class Primes {
-    public static void main(String[] args) {
-        // Replace this statement with your code
-    }
-}
+    public static void main(String[] args){
+        int N = Integer.parseInt(args[0]);
+
+        boolean[] boolarr = new boolean[N];
+
+        for (int index = 2; index <= boolarr.length-1 ; index++){
+            boolarr[index]=true;
+        }
+        int index = 3;
+        
+        while (index <= boolarr.length-1) {
+            for (int p = 2; p< boolarr.length-1 & p!=index; p++ )
+            if(index % p == 0 ){
+                boolarr[index]=false;
+            }
+            index++;
+            
+        }    
+               
+        for (int i = 0; i <= boolarr.length-1; i++) {
+            if(boolarr[i]==true){
+            System.out.println(i);
+            }
+
+        
+        }
+        int count = 0;
+        int g = 0;
+        while (g <= boolarr.length-1) {
+            if (boolarr[g]==true){
+                count++;
+             }
+             g++;
+        }
+        double total = N;
+        double calc = count/total*100;
+        int perc = (int) calc;
+       System.out.println("There are " + count + " primes between 2 and " + N + " (" + perc + "% are primes)");
+            
+            
+        
+    }}
